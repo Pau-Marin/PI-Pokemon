a9172a7157c90cb81cdc4ff81f35d69b88f0bf27
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-05 23:38:10 +0100

    Basic models

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,42 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define("pokemon", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+
+    hp: {
+      type: DataTypes.INTEGER,
+    },
+
+    atack: {
+      type: DataTypes.INTEGER,
+    },
+
+    defense: {
+      type: DataTypes.INTEGER,
+    },
+
+    speed: {
+      type: DataTypes.INTEGER,
+    },
+
+    height: {
+      type: DataTypes.INTEGER,
+    },
+
+    weight: {
+      type: DataTypes.INTEGER,
     },
   });
 };
