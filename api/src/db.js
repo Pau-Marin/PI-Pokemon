fad87fe003c40c2ffe8cfe31bb7cc1dd3bd3cf8c
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-05 23:47:29 +0100

    Models relations

--- api/src/db.js
+++ api/src/db.js
@@ -37,7 +37,10 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Pokemon, Type } = sequelize.models;
+
+Pokemon.hasMany(Type);
+Type.hasMany(Pokemon);
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
