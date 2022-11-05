21179283c02e19635bee5065bf7322b2b1860e5c
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-06 00:19:39 +0100

    Routes on own file

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
-const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
+const { Router } = require("express");
 
 const router = Router();
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const pokemonsRouter = require("./pokemons");
+const typesRouter = require("./types");
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/pokemons", pokemonsRouter);
+router.use("/types", typesRouter);
 
 module.exports = router;
