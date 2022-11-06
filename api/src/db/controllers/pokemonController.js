eebe262f7fbf7069ea05c587ad42d54c79079a06
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-06 21:55:07 +0100

    addPokemons now checks if pokemon already exist before adding

--- api/src/db/controllers/pokemonController.js
+++ api/src/db/controllers/pokemonController.js
@@ -2,11 +2,24 @@ const { Pokemon } = require("../db");
 
 module.exports = {
   // Añade un arreglo de objetos pokemons a la BBDD
-  addPokemons: function (pokemons) {
+  addPokemons: async function (pokemons) {
     if (!pokemons) throw new Error("No me mandaste pokemons para añadir!");
 
-    Pokemon.bulkCreate(pokemons);
-    return "Pokemons añadidos correctamente a la BBDD";
+    // let data = await Pokemon.bulkCreate(pokemons);
+    for (let i = 0; i < pokemons.length; i++) {
+      await Pokemon.findOrCreate({
+        where: { id: pokemons[i].id },
+        defaults: pokemons[i],
+        raw: true,
+      });
+    }
+
+    let pokeList = await this.listPokemons();
+
+    return {
+      data: pokeList.data,
+      msg: "Pokemons añadidos correctamente a la BBDD",
+    };
   },
 
   // Añade un objeto pokemon a la BBDD
@@ -16,7 +29,12 @@ module.exports = {
     Pokemon.create(pokemon);
   },
 
-  listPokemon: function () {},
+  // Lista todos los pokemons en la base de datos
+  listPokemons: async function () {
+    let results = await Pokemon.findAll({ raw: true });
+
+    return { data: results, msg: "Ya hay pokemons en la BBDD" };
+  },
 
   searchPokemon: function () {},
 };
