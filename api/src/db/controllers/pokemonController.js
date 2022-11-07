01477d3ecf0bf96916836afb203907f82ce18029
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-07 02:52:07 +0100

    Refactorization on pokemonController + route fix

--- api/src/db/controllers/pokemonController.js
+++ api/src/db/controllers/pokemonController.js
@@ -1,42 +1,17 @@
 const { Pokemon } = require("../db");
 
 module.exports = {
-  // Añade un arreglo de objetos pokemons a la BBDD
-  addPokemons: async function (pokemons) {
-    if (!pokemons) throw new Error("No me mandaste pokemons para añadir!");
-
-    let promises = [];
-    for (let i = 0; i < pokemons.length; i++) {
-      let promise = await Pokemon.findOrCreate({
-        where: { idAPI: pokemons[i].idAPI },
-        defaults: pokemons[i],
-        raw: true,
-      });
-      promises.push(promise);
-    }
-
-    Promise.all(promises).then((p) => {
-      console.log("All pokemons added to database");
-    });
-
-    let pokeList = await this.listPokemons();
-
-    return {
-      data: pokeList.data,
-      msg: "Pokemons añadidos correctamente a la BBDD",
-    };
-  },
-
   // Añade un objeto pokemon a la BBDD
   addPokemon: async function (pokemon) {
     if (!pokemon) throw new Error("No me mandaste un pokemon para añadir!");
 
-    let poke = await await Pokemon.findOrCreate({
+    let poke = await Pokemon.findOrCreate({
       where: { name: pokemon.name },
       defaults: pokemon,
       raw: true,
     });
-    return { data: poke, msg: "Pokemon añadido correctamente" };
+    console.log(`Pokemon ${pokemon.name} añadido correctamente`);
+    return { data: poke, msg: `Pokemon ${pokemon.name} añadido correctamente` };
   },
 
   // Lista todos los pokemons en la base de datos
