9955cff04adeade59b5e16fb3134bdf5a7dd7087
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-07 01:50:53 +0100

    Fixed /pokemon routes

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -5,6 +5,7 @@ const router = Router();
 
 // /pokemons/:id
 router.get("/:id", async (req, res) => {
+  console.log("/pokemon/:id called");
   const { id } = req.params;
   if (id) {
     try {
@@ -17,7 +18,8 @@ router.get("/:id", async (req, res) => {
 });
 
 // /pokemons?name=...
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
+  console.log("/pokemon/?query called");
   const { name } = req.query;
   if (name) {
     try {
@@ -27,11 +29,13 @@ router.get("/", async (req, res) => {
       return res.status(404).send(error.message);
     }
   }
+
+  next();
 });
 
-// /pokemons
+// /pokemons/
 router.get("/", async (req, res) => {
-  const { offset, limit } = req.query;
+  console.log("/pokemon/ called");
   try {
     let pokemons = await listPokemons();
     return res.json(pokemons);
