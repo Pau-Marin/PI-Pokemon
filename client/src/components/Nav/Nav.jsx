0bb26179e044f0268c61550d4a24b220437aa1a2
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-09 19:18:59 +0100

    NavBar

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -5,6 +5,9 @@ export default class Nav extends Component {
   render() {
     return (
       <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/types"}>Tipos</Link>
+        <Link to={"/createPokemon"}>Crear Pokemon</Link>
       </div>
     );
   }
