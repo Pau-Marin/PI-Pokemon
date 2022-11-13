29e55b85a16f15b9750da58b1c3725c04540efef
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-13 14:03:21 +0100

    Update CreatePokemon.jsx
    - Basic form

--- client/src/components/CreatePokemon/CreatePokemon.jsx
+++ client/src/components/CreatePokemon/CreatePokemon.jsx
@@ -1,11 +1,131 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllTypes } from "../../redux/actions";
 
-export default class CreatePokemon extends Component {
-  render() {
-    return (
+// export function validate(input) {
+//   let error = {};
+//   if (!input.username) {
+//     error.username = "Username is required";
+//   } else if (!/\S+@\S+\.\S+/.test(input.name)) {
+//     error.username = "Username is invalid";
+//   }
+
+//   if (!input.password) {
+//     error.password = "password is required";
+//   } else if (!/(?=.-*[0-9])/.test(input.password)) {
+//     error.password = "Password is invalid";
+//   }
+
+//   return error;
+// }
+
+export default function CreatePokemon() {
+  const dispatch = useDispatch();
+  const types = useSelector((state) => state.types);
+
+  useEffect(() => {
+    dispatch(getAllTypes());
+  }, [dispatch]);
+
+  // const [types, setTypes] = useState(0);
+  // const [error, setError] = React.useState({});
+  const [input, setInput] = React.useState({
+    name: "",
+    stats: [
+      { name: "HP", stat: 0 },
+      { name: "ATK", stat: 0 },
+      { name: "DEF", stat: 0 },
+      { name: "SPD", stat: 0 },
+    ],
+    types: { type1: "", type2: "" },
+    height: 0,
+    weight: 0,
+  });
+
+  let handleInputChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+
+    // let objError = validate({ ...input, [e.target.name]: e.target.value });
+    // setError(objError);
+  };
+
+  return (
+    <form>
+      <h1>Esto es Crear Pokemon</h1>
+      <label>Nombre del Pokemon:</label>
+      <input
+        type="text"
+        value={input.name}
+        onChange={handleInputChange}
+        name="name"
+        placeholder="Nombre del Pokemon"
+      ></input>
       <div>
-        <h1>Esto es Crear Pokemon</h1>
+        <h3>Estadísticas</h3>
+        <label>HP:</label>
+        <input
+          type="text"
+          value={input.stats[0].stat}
+          onChange={handleInputChange}
+          name="hp"
+          placeholder="Vida del Pokemon"
+        ></input>
+        <label>ATK:</label>
+        <input
+          type="text"
+          value={input.stats[1].stat}
+          onChange={handleInputChange}
+          name="atk"
+          placeholder="Ataque del Pokemon"
+        ></input>
+        <label>DEF:</label>
+        <input
+          type="text"
+          value={input.stats[2].stat}
+          onChange={handleInputChange}
+          name="def"
+          placeholder="Defensa del Pokemon"
+        ></input>
+        <label>SPD:</label>
+        <input
+          type="text"
+          value={input.stats[3].stat}
+          onChange={handleInputChange}
+          name="spd"
+          placeholder="Velocidad del Pokemon"
+        ></input>
       </div>
-    );
-  }
+      <div>
+        <h3>Tipos</h3>
+        <select>
+          {types.map((t) => {
+            return t.name !== "unknown" ? (
+              <option value={t.name} key={t.name}>
+                {t.name}
+              </option>
+            ) : null;
+          })}
+        </select>
+      </div>
+      <div>
+        <h3>Tamaño</h3>
+        <label>Height:</label>
+        <input
+          type="text"
+          value={input.height}
+          onChange={handleInputChange}
+          name="height"
+          placeholder="Altura del Pokemon"
+        ></input>
+        <label>Weight:</label>
+        <input
+          type="text"
+          value={input.weight}
+          onChange={handleInputChange}
+          name="weight"
+          placeholder="Peso del Pokemon"
+        ></input>
+      </div>
+    </form>
+  );
 }
