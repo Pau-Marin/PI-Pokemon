7c508849113dfbf4cf96974157e40e1cc2fbd32e
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-17 02:28:47 +0100

    Update Home.jsx
    Refactor

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,49 +1,38 @@
-import React, { Component } from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
-import * as actions from "../../redux/actions/index";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPokemons, getAllTypes } from "../../redux/actions/index";
 
 import logo from "../../img/logo.png";
 
 import PokeCard from "../PokeCard/PokeCard";
 import PokeSearch from "../PokeSearch/PokeSearch";
 
-export class Home extends Component {
-  componentDidMount() {
-    this.props.getAllPokemons();
-  }
+export default function Home() {
+  const dispatch = useDispatch();
+  const allPokemons = useSelector((state) => state.pokemons);
 
-  render() {
-    return (
-      <div className="home">
-        <h1>Esto es la Home</h1>
-        <img src={logo} alt="PIkemon logo" />
-        <PokeSearch />
-        {this.props.pokemons?.map((p) => {
-          return (
-            <PokeCard
-              key={p.id}
-              id={p.id}
-              name={p.name}
-              img={p.img}
-              stats={p.stats}
-              types={p.types}
-            />
-          );
-        })}
-      </div>
-    );
-  }
+  useEffect(() => {
+    dispatch(getAllPokemons());
+  }, [dispatch]);
+
+
+
+  return (
+    <div className="home">
+      <h1>Esto es la Home</h1>
+      <img src={logo} alt="PIkemon logo" />
+      <PokeSearch />
+      {currentPokemons?.map((p) => {
+        return (
+          <PokeCard
+            key={p.id}
+            id={p.id}
+            name={p.name}
+            img={p.img}
+            types={p.types}
+          />
+        );
+      })}
+    </div>
+  );
 }
-
-export const mapStateToProps = (state) => {
-  return {
-    pokemons: state.pokemons,
-  };
-};
-
-export const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(actions, dispatch);
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
