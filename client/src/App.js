adf2a3480b7ef9f558abb166ae2a24c883f35fa1
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-17 02:27:19 +0100

    Update App.js
    Refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
 
-import React, { Component } from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
+import React from "react";
+
 import { Route } from "react-router-dom";
-import * as actions from "./redux/actions";
 
 import Nav from "./components/Nav/Nav";
 import PokeLanding from "./components/PokeLanding/PokeLanding";
@@ -12,47 +10,14 @@ import Home from "./components/Home/Home";
 import PokeInfo from "./components/PokeInfo/PokeInfo";
 import CreatePokemon from "./components/CreatePokemon/CreatePokemon";
 
-export class App extends Component {
-  componentDidMount() {
-    this.props.getAllPokemons();
-  }
-
-  onFilter(pokeId) {
-    let pokemon = this.props.pokemons?.filter((p) => p.id === parseInt(pokeId));
-    if (pokemon.length > 0) {
-      return pokemon[0];
-    } else {
-      return null;
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <Route path={"/"} render={() => <Nav />} />
-        <Route exact path={"/"} render={() => <PokeLanding />} />
-        <Route exact path={"/home"} render={() => <Home />} />
-        <Route
-          exact
-          path={"/pokemon/:id"}
-          render={({ match }) => (
-            <PokeInfo pokemon={this.onFilter(match.params.id)} />
-          )}
-        />
-        <Route exact path={"/createPokemon"} render={() => <CreatePokemon />} />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <Route path={"/"} component={Nav} />
+      <Route exact path={"/"} component={PokeLanding} />
+      <Route exact path={"/home"} component={Home} />
+      <Route exact path={"/pokemon/:id"} component={PokeInfo} />
+      <Route exact path={"/createPokemon"} component={CreatePokemon} />
+    </div>
+  );
 }
-
-export const mapStateToProps = (state) => {
-  return {
-    pokemons: state.pokemons,
-  };
-};
-
-export const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(actions, dispatch);
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
