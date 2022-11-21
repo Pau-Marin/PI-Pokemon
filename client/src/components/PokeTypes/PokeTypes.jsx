fb6ef30bacb6c3e60329e4fe492a63ab8b3cfb8a
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-11-21 02:38:26 +0100

    Moved all pokemon types icons to a separated file

--- client/src/components/PokeTypes/PokeTypes.jsx
+++ client/src/components/PokeTypes/PokeTypes.jsx
@@ -3,49 +3,11 @@ import { useDispatch } from "react-redux";
 import { filterPokemonsByTypes } from "../../redux/actions/index";
 import { Link } from "react-router-dom";
 
-import bug from "../../img/icons/bug.svg";
-import dark from "../../img/icons/dark.svg";
-import dragon from "../../img/icons/dragon.svg";
-import electric from "../../img/icons/electric.svg";
-import fairy from "../../img/icons/fairy.svg";
-import fighting from "../../img/icons/fighting.svg";
-import fire from "../../img/icons/fire.svg";
-import flying from "../../img/icons/flying.svg";
-import ghost from "../../img/icons/ghost.svg";
-import grass from "../../img/icons/grass.svg";
-import ground from "../../img/icons/ground.svg";
-import ice from "../../img/icons/ice.svg";
-import normal from "../../img/icons/normal.svg";
-import poison from "../../img/icons/poison.svg";
-import psychic from "../../img/icons/psychic.svg";
-import rock from "../../img/icons/rock.svg";
-import steel from "../../img/icons/steel.svg";
-import water from "../../img/icons/water.svg";
+import { typeIcons } from "./icons";
 
-import "./pokeTypes.css";
+import "./PokeTypes.css";
 
 const PokeTypes = (props) => {
-  const typeIcons = {
-    bug,
-    dark,
-    dragon,
-    electric,
-    fairy,
-    fighting,
-    fire,
-    flying,
-    ghost,
-    grass,
-    ground,
-    ice,
-    normal,
-    poison,
-    psychic,
-    rock,
-    steel,
-    water,
-  };
-
   const { types } = props;
 
   const dispatch = useDispatch();
---
+++ client/src/components/PokeTypes/icons.js
@@ -0,0 +1,39 @@
+import bug from "../../img/icons/bug.svg";
+import dark from "../../img/icons/dark.svg";
+import dragon from "../../img/icons/dragon.svg";
+import electric from "../../img/icons/electric.svg";
+import fairy from "../../img/icons/fairy.svg";
+import fighting from "../../img/icons/fighting.svg";
+import fire from "../../img/icons/fire.svg";
+import flying from "../../img/icons/flying.svg";
+import ghost from "../../img/icons/ghost.svg";
+import grass from "../../img/icons/grass.svg";
+import ground from "../../img/icons/ground.svg";
+import ice from "../../img/icons/ice.svg";
+import normal from "../../img/icons/normal.svg";
+import poison from "../../img/icons/poison.svg";
+import psychic from "../../img/icons/psychic.svg";
+import rock from "../../img/icons/rock.svg";
+import steel from "../../img/icons/steel.svg";
+import water from "../../img/icons/water.svg";
+
+export const typeIcons = {
+  bug,
+  dark,
+  dragon,
+  electric,
+  fairy,
+  fighting,
+  fire,
+  flying,
+  ghost,
+  grass,
+  ground,
+  ice,
+  normal,
+  poison,
+  psychic,
+  rock,
+  steel,
+  water,
+};
